b7fb3182db6f8d2f7699daecd9bc8a15753e01f0
Zeyad Arafat <zeyadarafat833@gmail.com>
2026-03-13 01:27:10 +0200

    finished all categorical NB and small update in PCA

--- PCA.py
+++ PCA.py
@@ -1,10 +1,10 @@
 import numpy as np
 
 def pca(X, n_components):
-    X_normalized = X - np.mean(X, axis=0)
-    covariance_matrix = np.cov(X_normalized, rowvar=False)
-    eigenvalues, eigenvectors = np.linalg.eig(covariance_matrix)
+    X_centered = X - np.mean(X, axis=0)
+    covariance_matrix = np.cov(X_centered, rowvar=False)
+    eigenvalues, eigenvectors = np.linalg.eigh(covariance_matrix)
     idx = np.argsort(eigenvalues)[::-1][:n_components]
     principal_components = eigenvectors[:, idx]
-    X_projected = X_normalized @ principal_components
+    X_projected = X_centered @ principal_components
     return X_projected, principal_components
